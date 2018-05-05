0ecc19c8710d92741c646c70c67aecc26d9dd00c
Bernd Bestel <bernd@berrnd.de>
2018-05-05 17:18:35 +0200

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # deutsche-bahn-online-ticket-parser
 A little PowerShell script to parse Deutsche Bahn Online-Ticket PDFs
+
+## Why?
+I needed this, because I had a whole lot of tickets in PDF form and I wanted to have
+a list with the information for my Einkommensteuererklärung - unfortunetely, Deutsche Bahn
+does not provide any API or something for this - we call this Digitalisierung.
+
+This script reads Deutsche Bahn Online-Ticket PDFs from a folder,
+extracts various information and writes to them to the specified CSV file
+(see the script for paths and so on).
+
+## Example output
+```
+Auftrag: SXXXXQ
+Summe: 8,80
+Hinfahrt: Bobingen -> Egling
+Rueckfahrt: Egling -> Bobingen
+Datum: 11.10.2017
+Ticket-Typ: Flexpreis (Hin- und Rückfahrt)
+Reisende: 1, mit 1 BC50
+
+
+Auftrag: NXXXXD
+Summe: 4,40
+Hinfahrt: Bobingen -> Egling
+Datum: 12.10.2017
+Ticket-Typ: Flexpreis (Einfache Fahrt)
+Reisende: 1, mit 1 BC50
+```
+
+## Example CSV file
+```
+11.10.2017	Flexpreis (Hin- und Rückfahrt) (Reisende: 1, mit 1 BC50, Strecke: Bobingen -> Egling und Egling -> Bobingen, Auftragsnummer: SXXXXQ)	8,80
+12.10.2017	Flexpreis (Einfache Fahrt) (Reisende: 1, mit 1 BC50, Strecke: Bobingen -> Egling, Auftragsnummer: NXXXXD)	4,40
+```
+
+## Credits
+- PDF parsing: iTextSharp (https://github.com/itext/itextsharp)
+
+## License
+The MIT License (MIT)
---
+++ db-online-ticket-parser.ps1
@@ -0,0 +1,94 @@
+<#
+
+This script reads Deutsche Bahn Online-Ticket PDFs from a folder (see below),
+extracts various information and writes to them to the specified CSV file
+
+Author: Bernd Bestel (https://berrnd.de ; bernd@berrnd.de)
+Last updated: 2018-05-05
+Licence: MIT
+
+#>
+
+$FOLDER = "B:\Dokumente\Quittungen"
+$FILTER = "2017*Bahn Fahrkarte*.pdf"
+$OUTPUTFILE = "Fahrten.csv"
+
+
+Add-Type -Path itextsharp.dll
+
+ForEach ($file in Get-ChildItem -Path $FOLDER -Recurse -Filter $FILTER | Sort-Object Name)
+{
+	$auftrag = $null
+	$summe = $null
+	$hinfahrt = $null
+	$rueckfahrt = $null
+	$datum = $null
+	$ticketType = $null
+	$reisende = $null
+
+	$pdfReader = New-Object iTextSharp.Text.Pdf.PdfReader -ArgumentList $file.FullName
+	$pageLines = [iTextSharp.Text.Pdf.Parser.PdfTextExtractor]::GetTextFromPage($pdfReader, 1).Split([Environment]::NewLine)
+	$lineIndex = -1
+	foreach ($line in $pageLines)
+	{
+		$lineIndex++
+
+		if ($auftrag -eq $null -and $line.StartsWith("Auftragsnummer"))
+		{
+			$auftrag = $line.Split(" ")[1]
+		}
+		if ($summe -eq $null -and $line.StartsWith("Summe"))
+		{
+			$summe = $line.Split(" ")[1]
+			$summe = $summe.Remove($summe.Length - 1) #Remove Euro symbol at the end
+		}
+		if ($hinfahrt -eq $null -and $line.StartsWith("Hinfahrt"))
+		{
+			$lineParts = $line.Split(" ")
+			$hinfahrt = $lineParts[1] + " -> " + $lineParts[3]
+		}
+		if ($rueckfahrt -eq $null -and $line.Split(" ")[0].Contains("ckfahrt")) #Comparison to "Rückfahrt" does not work however
+		{
+			$lineParts = $line.Split(" ")
+			$rueckfahrt = $lineParts[1] + " -> " + $lineParts[3]
+		}
+		if ($datum -eq $null -and $line.Split(":")[0].Contains("ltig ab")) #Comparison to "Gültig ab" does not work however
+		{
+			$datum = $line.Split(" ")[2]
+		}
+		if ($ticketType -eq $null -and $line.Split(":")[0].Contains("ltigkeit")) #Comparison to "Gültigkeit" does not work however
+		{
+			$ticketType = $pageLines[$lineIndex + 1]
+		}
+		if ($reisende -eq $null -and $line.StartsWith("Erw:"))
+		{
+			$reisende = $line.Replace("Erw: ", "")
+		}
+	}
+	$pdfReader.Close()
+
+	Write-Host "Auftrag: $auftrag"
+	Write-Host "Summe: $summe"
+	Write-Host "Hinfahrt: $hinfahrt"
+	if ($rueckfahrt -ne $null)
+ 	{
+		Write-Host "Rueckfahrt: $rueckfahrt"
+	}
+	Write-Host "Datum: $datum"
+	Write-Host "Ticket-Typ: $ticketType"
+	Write-Host "Reisende: $reisende"
+	Write-Host
+	Write-Host
+
+	$strecke = $hinfahrt
+	if ($rueckfahrt -ne $null)
+ 	{
+		$strecke += " und $rueckfahrt"
+	}
+	$bezeichnung = "$ticketType (Reisende: $reisende, Strecke: $strecke, Auftragsnummer: $auftrag)"
+	$csvLine = "$datum	$bezeichnung	$summe"
+	$csvLine | Out-File -Append $OUTPUTFILE
+}
+
+Write-Host "Press any key to exit..."
+Read-Host
